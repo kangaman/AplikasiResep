c63df2c66431523f56d9737a7ae997ed1743a769
kangaman <saefulkinfo@gmail.com>
2025-11-28 16:00:41 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,8 @@
 <?php
+
+// Aktifkan kompresi output (mempercepat loading halaman)
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start();
+
 /**
  * PENGATURAN UNTUK LINGKUNGAN PRODUKSI (LIVE)
  * --------------------------------------------------
@@ -24,9 +28,9 @@ if (session_status() == PHP_SESSION_NONE) {
 
 // --- SESUAIKAN DENGAN INFORMASI DATABASE ANDA ---
 $db_host = 'localhost';
-$db_user = 'saefulba_resep'; // Ganti dengan username database Anda
-$db_pass = '&,(p_v[4NE37'; // Ganti dengan password database Anda
-$db_name = 'saefulba_resep'; // Ganti dengan nama database Anda
+$db_user = 'sssss'; // Ganti dengan username database Anda
+$db_pass = '7f{cB5sssS'; // Ganti dengan password database Anda
+$db_name = 'sssss'; // Ganti dengan nama database Anda
 // ----------------------------------------------------
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
